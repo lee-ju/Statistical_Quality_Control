da807540103bd3dc70ad6a856b6e20f9bb575002
Juhyun Lee <leeju@korea.ac.kr>
2022-02-09 15:58:37 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='Statistical_Quality_Control',
+    version='0.0.1',
+    description='SQC: for CJU',
+    url='https://github.com/lee-ju/Statistical_Quality_Control.git',
+    author='Juhyun Lee',
+    author_email='leeju@korea.ac.kr',
+    license='Juhyun Lee',
+    packages=['Statistical_Quality_Control'],
+    zip_safe=False,
+    install_requires=[]
+)
