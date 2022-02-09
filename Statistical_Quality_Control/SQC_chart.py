3bbf19660ac49204447b648062290777bbc1eb97
Juhyun Lee <leeju@korea.ac.kr>
2022-02-09 16:14:01 +0900

    Update SQC_chart.py

--- Statistical_Quality_Control/SQC_chart.py
+++ Statistical_Quality_Control/SQC_chart.py
@@ -28,3 +28,36 @@ def xbar_chart(D, A2):
     ax.set_title('X-bar chart')
     ax.set(xlabel='Group', ylabel='Average')
     return round(M, 4), round(UCL, 4), round(LCL, 4)
+
+def R_chart(D, D3, D4):
+    m = D.shape[0]
+    R = []
+    for i in range(m):
+        mi = D.iloc[i, 1:].values
+        R.append(mi.max() - mi.min())
+
+    Rbar = np.mean(R)
+    UCL = D4 * Rbar
+    LCL = D3 * Rbar
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.plot(R,
+            linestyle='-', marker='o', color='black')
+    ax.axhline(UCL,
+            linestyle='dashed', color='red')
+    ax.axhline(LCL,
+            linestyle='dashed', color='red')
+    ax.axhline(Rbar,
+            linestyle='dashed', color='blue')
+    ax.set_title('R chart')
+    ax.set(xlabel='Group', ylabel='Range')
+    return round(Rbar, 4), round(UCL, 4), round(LCL, 4)
+
+if __name__ == '__main__':
+    from Statistical_Quality_Control import SQC_chart
+    D = pd.read_csv('/content/drive/MyDrive/Colab Notebooks/Data/Wafer0.csv')
+    xbar_M, xbar_UCL, xbar_LCL = SQC_chart.xbar_chart(D=D, A2=0.577)
+    print(xbar_M, xbar_UCL, xbar_LCL)
+    
+    R_Rbar, R_UCL, R_LCL = R_chart(D, D3=0, D4=2.114)
+    print(R_Rbar, R_UCL, R_LCL)
