8fe7e295f3a3faf58f58e0eb750b87349e9b0284
Juhyun Lee <leeju@korea.ac.kr>
2022-02-22 14:28:19 +0900

    add Tools.py

---
+++ Statistical_Quality_Control/Tools.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+
+def plot_confusion_matrix(y_true, y_pred, classes,
+                                title=None,
+                                cmap=plt.cm.Blues):
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+
+    # Only use the labels that appear in the data
+    classes = np.array(classes)
+    classes = classes[unique_labels(y_true, y_pred)]
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = 'd' # fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    plt.show()
+    return ax
